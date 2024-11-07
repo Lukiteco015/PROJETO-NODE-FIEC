5589617d3196971b87fc1117160fad69391415b5
Eder-Moraes <moraeseder880@gmail.com>
2024-11-07 17:45:35 -0300

    Criação da conexão db, criação do user, authController, authRouters e o app

---
+++ back-end/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const bycrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    role: { type: String,  default: 'User' }
+})
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = await bycrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    return bycrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
