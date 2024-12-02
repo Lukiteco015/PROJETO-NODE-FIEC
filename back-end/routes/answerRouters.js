d680967f1eeae1ca8caddee4b045c18c290e488f
Eder-Moraes <moraeseder880@gmail.com>
2024-12-02 15:26:32 -0300

    Mudança nas permissões answerRouters

--- back-end/routes/answerRouters.js
+++ back-end/routes/answerRouters.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/', authMiddleware, answerController.createAnswer);
 
-router.get('/:feedbackId', authMiddleware, answerController.getAnswersByFeedbackId);
+router.get('/:feedbackId', answerController.getAnswersByFeedbackId);
 
 router.delete('/:id', authMiddleware, answerController.deleteAnswer);
 
